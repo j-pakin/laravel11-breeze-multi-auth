606b293f324e640bd00e2fda4fa9895d72c61738
j-pakin <janhuai@gmail.com>
2024-09-30 14:02:12 +0700

    Initial commit

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(!Auth::check()) {
+            return redirect()->route('login');
+        }
+
+        $userRole = Auth::user()->role;
+
+        if($userRole == 1) {
+            return $next($request);
+        }
+
+        elseif ($userRole == 2) {
+            return redirect()->route('engineer.dashboard');
+        }
+
+        elseif ($userRole == 3) {
+            return redirect()->route('user.dashboard');
+        }
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+//Route::get('/dashboard', function () {
+//    return Inertia::render('Dashboard');
+//})->middleware(['auth', 'verified'])->name('dashboard');
+
+// Admin dashboard
+Route::get('/admin/dashboard', function () {
+    return Inertia::render('Admin/Dashboard');
+})->middleware(['auth', 'verified',])->name('admin.dashboard');
+
+// Engineer dashboard
+Route::get('/engineer/dashboard', function () {
+    return Inertia::render('Engineer/Dashboard');
+})->middleware(['auth', 'verified'])->name('engineer.dashboard');
+
+// User dashboard
+Route::get('/user/dashboard', function () {
+    return Inertia::render('User/Dashboard');
+})->middleware(['auth', 'verified'])->name('user.dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
